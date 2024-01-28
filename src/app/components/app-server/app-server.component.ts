633714f1d4549c2fdd6d765ddfdf3670c739487b
Miguel Abreu <migabreu12@gmail.com>
2024-01-28 18:39:12 -0500

    24. Used different style of selector which is imbedded in a div element.

--- src/app/components/app-server/app-server.component.ts
+++ src/app/components/app-server/app-server.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-server',
+  // selector: 'app-server',
+  selector: '[app-server]',
   templateUrl: './app-server.component.html',
   styleUrls: ['./app-server.component.scss']
 })
