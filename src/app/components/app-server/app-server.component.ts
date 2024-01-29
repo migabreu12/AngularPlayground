53b102e9707e4b64c0d68882824681e4a996c9e8
Miguel Abreu <migabreu12@gmail.com>
2024-01-29 13:45:46 -0500

    28. Added notes for the different ways to use the selector.

--- src/app/components/app-server/app-server.component.ts
+++ src/app/components/app-server/app-server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server', //angular element
-  // selector: '[app-server∂]', // directive way
+  // selector: '[app-server∂]', // attribute way
   // selector: '.app-server', // css class way
   templateUrl: './app-server.component.html',
   styleUrls: ['./app-server.component.scss']
