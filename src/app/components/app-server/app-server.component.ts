fbfdb9fb7a4a1cf6780cf4bd5b40de6d5e9f65e0
Miguel Abreu <migabreu12@gmail.com>
2024-01-29 11:32:05 -0500

    reverted to the angular element way.

--- src/app/components/app-server/app-server.component.ts
+++ src/app/components/app-server/app-server.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  //selector: 'app-server', //angular element
+  selector: 'app-server', //angular element
   // selector: '[app-server∂]', // directive way
-  selector: '.app-server', // css class way
+  // selector: '.app-server', // css class way
   templateUrl: './app-server.component.html',
   styleUrls: ['./app-server.component.scss']
 })
