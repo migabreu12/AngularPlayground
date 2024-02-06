e1a45375f491b30ba53cc1ecd83e046c812b2664
Miguel Abreu <migabreu12@gmail.com>
2024-02-06 04:12:41 -0500

    Assignment 4. Interim Commit: The previous example did not work because 'this' was not bound to the correct instance. Using the anonymous method declaration fixed this issue

--- src/app/components/assignment4/game-control/game-control.component.ts
+++ src/app/components/assignment4/game-control/game-control.component.ts
@@ -1,26 +1,23 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
   styleUrls: ['./game-control.component.scss']
 })
-export class GameControlComponent implements OnInit {
+export class GameControlComponent {
   @Output() startClicked = new EventEmitter<any>();
   @Output() stopClicked = new EventEmitter<any>();
-  @Output() valueEmitter = new EventEmitter<any>();
+  @Output() valueEmitted = new EventEmitter<any>();
 
   private intervalStorage: any = null;
-
-  public ngOnInit(): void {
-    this.valueEmitter = new EventEmitter<any>();
-  }
 
   public emitStartClicked(): void {
     this.startClicked.emit();
     if(!!!this.intervalStorage) {
-      // Problem is here
-      // this.intervalStorage = setInterval(this.emitValue, 1000);
+      this.intervalStorage = setInterval(() => {
+        this.valueEmitted.emit(this.getRandomNumber(0, 10));
+      }, 1000);
     }
   }
 
@@ -28,11 +25,6 @@ export class GameControlComponent implements OnInit {
     this.stopClicked.emit();
     clearInterval(this.intervalStorage);
     this.intervalStorage = null;
-  }
-
-  public emitValue(): void {
-    // console.log(this.getRandomNumber(0, 10));
-    this.valueEmitter.emit();
   }
 
   private getRandomNumber(min: number, max: number): number {
