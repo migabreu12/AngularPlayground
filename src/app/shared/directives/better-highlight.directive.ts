b6af4fb15ae542a73e1c713a7c0649d3c68d3f3b
Miguel Abreu <migabreu12@gmail.com>
2024-02-07 13:35:12 -0500

    101. Implemented a way for the user to update the color of the directive using Inputs. Added notes regarding using inputs for the directive with special cases and issues.

--- src/app/shared/directives/better-highlight.directive.ts
+++ src/app/shared/directives/better-highlight.directive.ts
@@ -1,13 +1,25 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultBackgroundColor = 'transparent';
+  @Input() highlightBackgroundColor = 'blue';
+  @Input() defaultColor = 'black';
+  @Input() highlightColor = 'white';
+
   // This is another way to adjust the styling of an element rather than using the renderer.
   // It let's us create class properties that bind to the element's properties
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
-  @HostBinding('style.color') color = 'black';
+  // Assigning the value of the background color here (before ngOnInit) will set the background color on first page load
+  // to transperent all the time (regardless if it was overwritten) because we, at this point, do not have the
+  // input value from external sources. The assigment, to fix this, should be done in ngOnInit.
+  // Cycle goes custom -> native.
+  // You can also assign the input value an alias of the directive name and then assign the value there too. For example
+  // assigning highlightBackgroundColor alias to @Input('appBetterHighlight') highlightBackgroundColor and then in the element using
+  // the directive like [appBetterHighlight]="'blue'".
+  @HostBinding('style.backgroundColor') backgroundColor;
+  @HostBinding('style.color') color;
 
   constructor(
       private elementRef: ElementRef,
@@ -17,20 +29,22 @@ export class BetterHighlightDirective implements OnInit {
   public ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "blue");
     // this.renderer.setStyle(this.elementRef.nativeElement, "color", "white");
+    this.backgroundColor = this.defaultBackgroundColor;
+    this.color = this.defaultColor;
   }
 
   // Using the host listener decorator is a nice tool to use for dynamic styling
   @HostListener('mouseenter') mouseEnter(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "blue");
     // this.renderer.setStyle(this.elementRef.nativeElement, "color", "white");
-    this.backgroundColor = "blue";
-    this.color = "white";
+    this.backgroundColor = this.highlightBackgroundColor;
+    this.color = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "transparent");
     // this.renderer.setStyle(this.elementRef.nativeElement, "color", "black");
-    this.backgroundColor = "transparent";
-    this.color = "black";
+    this.backgroundColor = this.defaultBackgroundColor;
+    this.color = this.defaultColor;
   }
 }
