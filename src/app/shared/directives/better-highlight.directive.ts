4939e9689c9566e7b77c030faf094d693a017013
Miguel Abreu <migabreu12@gmail.com>
2024-02-07 13:16:35 -0500

    100. Using the host binding decorator to create local properties for the background color and text color. Using host binding replaces having to use the renderer for element properties.

--- src/app/shared/directives/better-highlight.directive.ts
+++ src/app/shared/directives/better-highlight.directive.ts
@@ -1,9 +1,13 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // This is another way to adjust the styling of an element rather than using the renderer.
+  // It let's us create class properties that bind to the element's properties
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @HostBinding('style.color') color = 'black';
 
   constructor(
       private elementRef: ElementRef,
@@ -15,13 +19,18 @@ export class BetterHighlightDirective implements OnInit {
     // this.renderer.setStyle(this.elementRef.nativeElement, "color", "white");
   }
 
+  // Using the host listener decorator is a nice tool to use for dynamic styling
   @HostListener('mouseenter') mouseEnter(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "blue");
-    this.renderer.setStyle(this.elementRef.nativeElement, "color", "white");
+    // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "blue");
+    // this.renderer.setStyle(this.elementRef.nativeElement, "color", "white");
+    this.backgroundColor = "blue";
+    this.color = "white";
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "transparent");
-    this.renderer.setStyle(this.elementRef.nativeElement, "color", "black");
+    // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "transparent");
+    // this.renderer.setStyle(this.elementRef.nativeElement, "color", "black");
+    this.backgroundColor = "transparent";
+    this.color = "black";
   }
 }
