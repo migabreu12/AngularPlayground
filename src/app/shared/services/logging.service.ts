df84a8c49ef3387e1c2c7c90d232252302864317
Miguel Abreu <migabreu12@gmail.com>
2024-02-11 15:04:45 -0500

    117. Added notes regarding lazy loading and using providedIn: 'root'

--- src/app/shared/services/logging.service.ts
+++ src/app/shared/services/logging.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+// Using the following way to import a service into the app module is a better way than traditional methods (using providers) since
+// this import allows for lazy loading.
 @Injectable({
   providedIn: 'root'
 })
