00393ec4ec3de7bdbd7ff12b4071e71d17d1fab3
Miguel Abreu <migabreu12@gmail.com>
2024-02-22 14:54:50 -0500

    135. Added an example of the relative path (programmatical approach) with comments and descriptions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,19 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'angular-playground';
 
-  public constructor(private router: Router) {
+  public constructor(private router: Router, private route: ActivatedRoute) {
   }
 
   public onGoToAngularPlayground(): void {
     this.router.navigate(["/"])
+  }
+
+  public goToAssignment4RelativeExample(): void {
+    // This let's you use relative path via relative to the component (and the component's current route)
+    // Because relativeTo: this.route uses the component's current "path" via routing, it will always work correctly since it's current
+    // route will always be "".
+    // Here's the explanation on relative to:
+    // "Relative to" means relative to the component in which this navigation is defined, not relative to the url which is currently displayed in the browser's address bar (which is not necessarily the same).
+    this.router.navigate(["assignment4"], { relativeTo: this.route });
   }
 }
