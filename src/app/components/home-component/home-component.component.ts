c871903f5d25ba489980d7d3a4e168e16c3cfd9e
Miguel Abreu <migabreu12@gmail.com>
2024-02-23 09:34:44 -0500

    138. Added an example of being able to subscribe to the params observable and update component values when a component does not reload itself. Added description comments.

--- src/app/components/home-component/home-component.component.ts
+++ src/app/components/home-component/home-component.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
@@ -13,18 +13,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomeComponentComponent {
   public parameterPassedInViaPath: { id: number, name: string };
+  public parametersChangedAfterReloadingComponent: { id: number, name: string };
 
   public constructor(private route: ActivatedRoute, private router: Router) {
     this.parameterPassedInViaPath = {
+      // Using the route.snapshot is okay for on load data assignment
       id: this.route.snapshot.params["id"],
       name: this.route.snapshot.params["name"]
     }
+    this.route.params
+      .subscribe((params: Params) => {
+        this.parametersChangedAfterReloadingComponent = {
+          id: params["id"],
+          name: params["name"]
+        }
+      });
   }
 
   public exampleOfNotReloadingComponentWithParameters() {
     // This example shows that the component does not reload (by angular default) because we're already
     // on the component; Not reloading the component is desired functionality to not waste resources.
     // The expected behavior is that the path will update but the displayed parameters will not.
-    this.router.navigate(["/home", 5, "nine"]);
+    this.router.navigate(["/home", Math.random(), "nine"]);
   }
 }
