5ec40562cfb8e8b6ff177b2b1218074641bb901f
Miguel Abreu <migabreu12@gmail.com>
2024-03-06 15:32:55 -0500

    206. Implemented the example code onto the reactive form example component to follow along.

--- src/app/components/forms-example/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/forms-example/reactive-form-example/reactive-form-example.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactive-form-example.component.scss']
 })
 export class ReactiveFormExampleComponent {
-
+  public genders = ['male', 'female'];
 }
