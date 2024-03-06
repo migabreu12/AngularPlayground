bd53226a962392d3f261023349ddc88479e6586c
Miguel Abreu <migabreu12@gmail.com>
2024-03-06 11:27:02 -0500

    202. Implemented setting values to the form (either the whole form or part of the form) via the code behind.

--- src/app/components/forms-example/forms-example.component.ts
+++ src/app/components/forms-example/forms-example.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['forms-example.component.scss']
 })
 export class FormsExampleComponent {
-  @ViewChild('f') form: NgForm
+  @ViewChild('f') exampleForm: NgForm
   public defaultQuestion = "pet";
   public answer = "";
   public genders = [
@@ -19,7 +19,40 @@ export class FormsExampleComponent {
   //   console.log(form);
   // }
 
+  public suggestUserName() {
+    const suggestedName = "Superuser";
+    // Must pass the exact value of the form and override all values
+    // this.exampleForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ""
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male"
+    // });
+
+    this.exampleForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
+  }
+
+  public setDefaultValues() {
+    // Must pass the exact value of the form and override all values
+    this.exampleForm.setValue({
+      userData: {
+        username: "Default",
+        email: ""
+      },
+      secret: "pet",
+      questionAnswer: "",
+      gender: "male"
+    });
+  }
+
   public onSubmit() {
-    console.log(this.form);
+    console.log(this.exampleForm);
   }
 }
