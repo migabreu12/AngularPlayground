5139fd666ab0adcbf3fc5f0accdf1266cb021db7
Miguel Abreu <migabreu12@gmail.com>
2024-03-05 19:00:24 -0500

    191. Added an example of passing the form data to a method and assigning the submit method to a form.

--- src/app/components/forms-example/forms-example.component.ts
+++ src/app/components/forms-example/forms-example.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-example',
   templateUrl: './forms-example.component.html'
 })
 export class FormsExampleComponent {
-
+  public onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
