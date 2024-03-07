d3917263a1986ead7ffa7426ece2069756249cdc
Miguel Abreu <migabreu12@gmail.com>
2024-03-07 12:48:31 -0500

    214. Implemented the dynamic components example which adds a new form control to the form group object via a form array.

--- src/app/components/forms-example/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/forms-example/reactive-form-example/reactive-form-example.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-example',
@@ -18,11 +18,26 @@ export class ReactiveFormExampleComponent implements OnInit {
         "username": new FormControl(null, Validators.required),
         "email": new FormControl(null, [Validators.required, Validators.email]),
       }),
-      "gender": new FormControl("male")
+      "gender": new FormControl("male"),
+      "hobbies": new FormArray([])
     })
   }
 
   public onSubmit() {
     console.log(this.exampleForm);
+  }
+
+  public addHobby() {
+    // Have to typecast to FormArray so we can have access to the push method; I feel like there's a different way to do this
+    // that doesn't involve typecasting.
+    const emptyHobby = new FormControl(null, Validators.required);
+    // This push adds a new control to the example form object (which is interactable by the user); What this means is that the form
+    // control can be updated and editated at any time and the example form will retain the changed value (alongside any validators. etc)
+    (<FormArray>this.exampleForm.get("hobbies")).push(emptyHobby);
+  }
+
+  // This method is needed because the example code attempts to get the hobbies in the template (which is not allowed anymore)
+  public getHobbyControls(): AbstractControl<any, any>[] {
+    return (<FormArray>this.exampleForm.get("hobbies")).controls;
   }
 }
