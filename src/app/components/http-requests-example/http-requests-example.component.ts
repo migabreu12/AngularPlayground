e2b41f447a3bc32146fb0a6dbf6a9ba6b34b2b98
Miguel Abreu <migabreu12@gmail.com>
2024-03-13 13:59:02 -0400

    266. Created and implemented the post service that handles fetching and uploading posts to the db. Refactored the http requests example to use the new post service.

--- src/app/components/http-requests-example/http-requests-example.component.ts
+++ src/app/components/http-requests-example/http-requests-example.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs';
 import { Post } from './models/post.model';
+import { PostService } from 'src/app/shared/services/post.service';
 
 @Component({
   selector: 'app-http-requests-example',
@@ -12,61 +13,30 @@ export class HttpRequestsExampleComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postService: PostService) {}
 
-  ngOnInit() {
+  public ngOnInit() {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: Post) {
-    // Send Http request
-    // This observable will auto clean up itself (unsubscribe) since
-    // it's provided by angular's default library.
-    // Observables will not fire/trigger unless there's a subscription
-    this.http.post<{ name: string }>(
-      "https://udemyangularcourse-16627-default-rtdb.firebaseio.com/posts.json",
-      postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+  public onCreatePost(postData: Post) {
+    this.postService.createAndStorePost(postData.title, postData.content);
   }
 
-  onFetchPosts() {
-    // Send Http request
+  public onFetchPosts() {
     this.fetchPosts();
-  }
-
-  onClearPosts() {
-    // Send Http request
   }
 
   private fetchPosts() {
+    // Send Http request
     this.isFetching = true;
-    this.http
-      // This is another way to assign the type of the response data (before it even gets to pipes)
-      // The <...> after the get can be removed to be of type any
-      // Setting the response data type using <...> is available on all types of requests
-      .get<{ [key: string]: Post }>("https://udemyangularcourse-16627-default-rtdb.firebaseio.com/posts.json")
-      .pipe(
-        // THis is one way to assign the type of the response data
-        // map((responseData: { [key: string]: Post }) => {
-        map(responseData => {
-          const postsArray: Post[] = [];
-          for(let key in responseData) {
-            if(responseData.hasOwnProperty(key)) {
-              // Creates a new object where ... then flattens the existing object to be individual
-              // objects that will be added as a property of the new object we're creating.
-              // the Key is a new property we're adding to the new object.
-              postsArray.push({ ...responseData[key], id: key });
-            }
-          }
+    this.postService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
+  }
 
-          return postsArray;
-        })
-      )
-      .subscribe(posts => {
-        this.loadedPosts = posts;
-        this.isFetching = false;
-      })
+  public onClearPosts() {
+    // Send Http request
   }
 }
